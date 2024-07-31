69fa8a944e92690fac40407403a7c317aaeb7bc0
Aleksei Balan <28422872+abalanonline@users.noreply.github.com>
2024-07-30 20:12:03 -0400

    2024-07-31 00:12:03+00:00

--- src/main/java/ab/tui/TuiConsole.java
+++ src/main/java/ab/tui/TuiConsole.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2024 Aleksei Balan
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package ab.tui;
 
 import com.googlecode.lanterna.TerminalSize;
@@ -171,6 +188,9 @@ public class TuiConsole implements Tui, Runnable {
       case ArrowUp:
       case ArrowRight:
         keyNotation.append(key.getKeyType().toString().substring(5));
+        break;
+      case ReverseTab:
+        keyNotation.append("Shift+Tab");
         break;
       default:
         keyNotation.append(key.getKeyType());
